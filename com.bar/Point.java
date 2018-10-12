6b5c7e6d11b9d376199fc47ccfa3a680ef6333d6
nazish <Nazish.Asad@knapp.com>
2018-10-12 12:10:52 +0200

    com.bar package has been added

---
+++ com.bar/Point.java
@@ -0,0 +1,43 @@
+public class Point 
+{
+	private int x;
+	private int y;
+	public Point(int x,int y) 
+	{
+		this.x = x;
+		this.y = y;
+	}
+	@Override
+	public String toString() 
+	{
+		return "("+x+","+y+")";
+	}
+	public static void main(String ... args) 
+	{
+		Point p1 = new Point(10,20);
+		Point p2 = new Point(10,20);
+		System.out.println(p1);
+		System.out.println(p1.(p2));
+	}	
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if(obj != null)
+		{
+			if(obj instanceof Point) 
+			{
+				Point that = (Point) obj;
+				if(this == that) 
+				{
+					return true;
+				}
+				if(this.x == that.x && this.y == that.y) 
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+}
